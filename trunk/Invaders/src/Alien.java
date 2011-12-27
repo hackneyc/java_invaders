f713eeb4e016f04094fff2b0594b0bdd7c366ff0
chackney <chackney@25f438c6-0afb-43ab-8e3b-b803e82d3985>
2011-12-27 20:10:47 +0000

    Merged changes from Alien_Explode branch.

--- trunk/Invaders/src/Alien.java
+++ trunk/Invaders/src/Alien.java
@@ -4,24 +4,48 @@ import java.awt.Graphics;
 public class Alien extends Sprite {
 	
 	public Sprite missile;
+	public Sprite explode;
 	private int points;
+	private int dieCount;
 	
 	public Alien(int xPos, int yPos, int startFrame, String fileName,
 			int maxFrames, int fps, int scaleX, int scaleY, Dimension d) {
 		super(xPos, yPos, startFrame, fileName, maxFrames, fps, scaleX, scaleY, d);
 		missile = new Sprite(0, 0, 0, "missile", 2, 0, 4, 24, d);
+		explode = new Sprite(0, 0, 0, "alien_explode_", 1, 0, 16, 16, d);
 		reset();
 	}	
 
 	public void reset() {
 		super.reset();
-		missile.visible = false;
+		missile.setVisible(false);
+		explode.setVisible(false);
+	}
+	
+	public void setDying()
+	{
+		setVisible(false);
+		dieCount = 0;
+		explode.setX((getX() + (getWidth() / 2)) - (explode.getWidth()/2));
+		explode.setY((getY() + (getHeight() / 2)) - (explode.getHeight()/2));
+		explode.setVisible(true);
 	}
 	
 	public void draw(Graphics g)
 	{
 		super.draw(g);
 		missile.draw(g);
+		if (explode.visible)
+		{
+			if (dieCount++ > 20)
+			{
+				explode.setVisible(false);
+			}
+			else
+			{
+				explode.draw(g);
+			}
+		}
 	}
 	
 	public boolean shoot()
