6439378cefd8644620d339497ad032c24c227076
chackney <chackney@25f438c6-0afb-43ab-8e3b-b803e82d3985>
2011-12-28 05:47:53 +0000

    Updated comments and code clean up.

--- trunk/Invaders/src/Alien.java
+++ trunk/Invaders/src/Alien.java
@@ -6,22 +6,42 @@ public class Alien extends Sprite {
 	public Sprite explode;
 	private int points;
 	private int dieCount;
-
+	private Dimension dim;
+	
+	/**
+	 * Constructor for a new alien.
+	 * @param xPos
+	 * @param yPos
+	 * @param startFrame
+	 * @param fileName
+	 * @param maxFrames
+	 * @param fps
+	 * @param scaleX
+	 * @param scaleY
+	 * @param d
+	 */
 	public Alien(int xPos, int yPos, int startFrame, String fileName,
 			int maxFrames, int fps, int scaleX, int scaleY, Dimension d) {
-		super(xPos, yPos, startFrame, fileName, maxFrames, fps, scaleX, scaleY,
-				d);
-		missile = new Sprite(0, 0, 0, "missile", 2, 0, 4, 24, d);
-		explode = new Sprite(0, 0, 0, "alien_explode_", 1, 0, 16, 16, d);
+		super(xPos, yPos, startFrame, fileName, maxFrames, fps, scaleX, scaleY);
+		missile = new Sprite(0, 0, 0, "missile", 2, 0, 4, 24);
+		explode = new Sprite(0, 0, 0, "alien_explode_", 1, 0, 16, 16);
+		dim = d;
 		reset();
 	}
 
+	/**
+	 * Reset the alien and hide its missile and explosion sprite.
+	 */
 	public void reset() {
 		super.reset();
 		missile.setVisible(false);
 		explode.setVisible(false);
 	}
 
+	/**
+	 * Hide the alien, reset the explosion timer and set the x/y
+	 * coordinate of the explosion.
+	 */
 	public void setDieing() {
 		setVisible(false);
 		dieCount = 0;
@@ -30,6 +50,11 @@ public class Alien extends Sprite {
 		explode.setVisible(true);
 	}
 
+	/**
+	 * Display the alien and its missile. If the alien is exploding
+	 * keep track of how long the explosion is visible.
+	 * @param g Graphics context.
+	 */
 	public void draw(Graphics g) {
 		super.draw(g);
 		missile.draw(g);
@@ -42,16 +67,25 @@ public class Alien extends Sprite {
 		}
 	}
 
+	/**
+	 * Initiate the firing of this aliens missile.
+	 * @return true if the alien started to shoot a missile else false.
+	 */
 	public boolean shoot() {
 		if (isVisible() && !missile.isVisible()) {
 			// New missile
 			missile.setVisible(true);
 			missile.setX((getX() + (getWidth() / 2)) - (missile.getWidth() / 2));
 			missile.setY(getY() + getHeight());
+			return(true);
 		}
-		return (missile.isVisible());
+		return (false);
 	}
 
+	/**
+	 * Update the missile position for this alien.
+	 * @return true if the alien missile is active else false.
+	 */
 	public boolean updateMissile() {
 		if (missile.isVisible()) {
 			if ((missile.getY() + 8) < dim.height) {
@@ -64,10 +98,18 @@ public class Alien extends Sprite {
 		return (missile.isVisible());
 	}
 
+	/**
+	 * Obtain the number of points for hitting this alien.
+	 * @return points.
+	 */
 	int getPoints() {
 		return (points);
 	}
 
+	/**
+	 * Set the number of points for this alien.
+	 * @param points Number of points for this alien.
+	 */
 	void setPoints(int points) {
 		this.points = points;
 	}
