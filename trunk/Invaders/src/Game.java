ca498c300c91873ba017e74e785272123f8f7aab
chackney <chackney@25f438c6-0afb-43ab-8e3b-b803e82d3985>
2011-12-28 02:14:57 +0000

    Added level number display at the start of each level. Added "Game Over" display when all lives have been used. Speed of aliens is increased with each level.

--- trunk/Invaders/src/Game.java
+++ trunk/Invaders/src/Game.java
@@ -12,8 +12,6 @@ public class Game extends JFrame implements Runnable {
 
 	private Thread updateThread;
 	private Base base;
-	private Score score;
-	private Lives lives;
 	private Insets insets;
 	private Dimension dim;
 	private Shield shield[];
@@ -29,8 +27,7 @@ public class Game extends JFrame implements Runnable {
 		
 		for(i=0; i<3; i++)
 		{
-			shield[i] = new Shield();
-			shield[i].setXY(100+(i*200)+insets.left, 375);
+			shield[i] = new Shield(100+(i*200)+insets.left, 375);
 		}
 	}
 	
@@ -55,9 +52,6 @@ public class Game extends JFrame implements Runnable {
 		//
 		// Things that are reset per game
 		//
-		score = new Score(insets.left + 100, insets.top + 20);
-		
-		lives = new Lives(400, insets.top + 20);
 		createSprites();
 
 		addKeyListener(base);
@@ -86,60 +80,122 @@ public class Game extends JFrame implements Runnable {
 
 	@Override
 	public void run() {
+		int speed = 70;
+		Score score;
+		Lives lives;
+		Font font;
 		int i;
+		int level = 0;
 		BufferStrategy bf = getBufferStrategy();
-		Graphics g = bf.getDrawGraphics();
-		Font font = new Font("Verdana", Font.BOLD, 24);
-		g.setFont(font);
+		Graphics2D g = (Graphics2D) bf.getDrawGraphics();
+				
 		g.setColor(Color.GREEN);
-		
-		while ((Thread.currentThread() == updateThread) &&
-			   (aliens.numAlive() != 0) &&
-			   (lives.get() > 0)) {
+		score = new Score(insets.left + 100, insets.top + 20);		
+		lives = new Lives(400, insets.top + 20);
 
+		while ((Thread.currentThread() == updateThread) &&
+				   (lives.get() > 0)) {
 			g.clearRect(insets.left, insets.top, dim.width - insets.left - insets.right, dim.height - insets.bottom - insets.top);
 
-			base.updatePosition();
-			aliens.updatePosition();
-			aliens.updateMissiles();
-
-			//
-			// Collision checking
-			// 
-			score.add(base.collision(aliens));
+			base.reset();
+			aliens.reset();
+			
+			if(speed >= 10)
+				speed -= 10;
+			aliens.setAlienSpeed(speed);
 			
 			for(i=0; i<3; i++)
 			{
-				base.collision(shield[i]);
-				aliens.collision(shield[i]);
+				shield[i].reset();
 				shield[i].draw(g);
 			}
-
-			if (aliens.collision(base))
-			{
-				lives.decrement();
-			}
 			
-			aliens.draw(g);
-			base.draw(g);
+			font = new Font("Verdana", Font.BOLD, 32);
+			g.setFont(font);
+			level++;
+			g.drawString(String.format("Level %d", level), 250, 200);
+
+			font = new Font("Verdana", Font.BOLD, 24);
+			g.setFont(font);	
 			score.draw(g);
 			lives.draw(g);
-
-			// Wait for vertical retrace then update the screen
-			VideoSync.waitForBeginOfVerticalBlank();
-			// Make the buffer visible
+			
 			bf.show();
-			// Force the buffer to the screen
-			Toolkit.getDefaultToolkit().sync();
-/*			
+			
 			try {
-				// Give other things time to do stuff.
-				Thread.sleep(10);
+				Thread.sleep(1500);
 			} catch (InterruptedException e) {
-				break;
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-*/
+			
+			while ((Thread.currentThread() == updateThread) &&
+				   (aliens.numAlive() != 0) &&
+				   (lives.get() > 0)) {
+	
+				g.clearRect(insets.left, insets.top, dim.width - insets.left - insets.right, dim.height - insets.bottom - insets.top);
+	
+				base.updatePosition();
+				aliens.updatePosition();
+				aliens.updateMissiles();
+	
+				//
+				// Collision checking
+				// 
+				score.add(base.collision(aliens));
+				
+				for(i=0; i<3; i++)
+				{
+					base.collision(shield[i]);
+					aliens.collision(shield[i]);
+					shield[i].draw(g);
+				}
+	
+				if (aliens.collision(base))
+				{
+					try {
+						Thread.sleep(1500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					lives.decrement();
+				}
+				
+				aliens.draw(g);
+				base.draw(g);
+				score.draw(g);
+				lives.draw(g);
+	
+				// Wait for vertical retrace then update the screen
+				VideoSync.waitForBeginOfVerticalBlank();
+				// Make the buffer visible
+				bf.show();
+				// Force the buffer to the screen
+				Toolkit.getDefaultToolkit().sync();
+	/*			
+				try {
+					// Give other things time to do stuff.
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					break;
+				}
+	*/
+			}
 		}
+		g.setColor(Color.RED);
+		font = new Font("Verdana", Font.BOLD, 32);
+		g.setFont(font);
+		g.drawString("Game Over", 250, 200);
+		bf.show();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		g.dispose();
 	}
 }
