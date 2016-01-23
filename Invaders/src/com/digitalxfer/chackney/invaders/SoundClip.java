811b2c0ab1c18329034c85ea54fa027f7932d2ec
chackney <chackney@25f438c6-0afb-43ab-8e3b-b803e82d3985>
2016-01-23 19:16:10 +0000

    Fixed playing of wav samples.

--- Invaders/src/com/digitalxfer/chackney/invaders/SoundClip.java
+++ Invaders/src/com/digitalxfer/chackney/invaders/SoundClip.java
@@ -1,24 +1,31 @@
 package com.digitalxfer.chackney.invaders;
 
 import java.io.IOException;
-import java.io.InputStream;
+
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import java.net.URL;
 
 public class SoundClip {
 	private Clip clip = null;
 
 	public SoundClip(String fileName) {
-		InputStream stream = ClassLoader.getSystemResourceAsStream(fileName);
-		AudioInputStream sound;
 		try {
-			if ((sound = AudioSystem.getAudioInputStream(stream)) != null) {
-				clip = AudioSystem.getClip();
-				clip.open(sound);
-			}
+			// Convert the filename to a URL
+	        URL url = this.getClass().getClassLoader().getResource(fileName);
+	        // Display the URL
+			System.out.println("Loading audio clip: " + url);
+			
+	  		AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+            AudioFormat format = audioIn.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            clip = (Clip)AudioSystem.getLine(info);
+			clip.open(audioIn);
 		} catch (UnsupportedAudioFileException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
